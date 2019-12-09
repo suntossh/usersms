31125a8590f6aa82b0c32d250ed36b1190bd8515
suntossh.numerouno@gmail.com <suntossh.numerouno@gmail.com>
2019-12-08 22:38:14 -0500

    updated WebSecurity with allowing Request from APIGateway IPConfig, means IP Adress where APIGateway is running, reading IP Config from Properties file

--- src/main/java/com/suntossh/springboot/users/security/WebSecurity.java
+++ src/main/java/com/suntossh/springboot/users/security/WebSecurity.java
@@ -1,6 +1,8 @@
 package com.suntossh.springboot.users.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,10 +11,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	Environment environment;
+	
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();// Not 100% sure
 		http.headers().frameOptions().disable();// enables h2 console to display
-		http.authorizeRequests().antMatchers("/users/**").permitAll();// this will allow un authenticated following url
+		http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"));// this will allow un authenticated following url
 																		// patters to be allowed
 	}
 
--- src/main/java/com/suntossh/springboot/users/service/UsersServiceImpl.java
+++ src/main/java/com/suntossh/springboot/users/service/UsersServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.suntossh.springboot.users.Entity.UsersEntity;
@@ -17,9 +18,12 @@ public class UsersServiceImpl implements UsersService {
 	@Autowired
 	UserRepository userRepository;
 	
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	
 	@Override
 	public UsersDto create(UsersDto users) {
-		users.setEncryptedPwd("TEST ENC PWD");
+		users.setEncryptedPwd(bCryptPasswordEncoder.encode(users.getPassword()));
 		users.setUserid(UUID.randomUUID().toString());
 		ModelMapper mapper = new ModelMapper();
 		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
